0ff957e5a27a87fd4257f677085ce26186744577
Marat Kudinov <marat.kudinov@orioninc.com>
2022-10-26 00:29:14 +0200

    suddenly, done

---
+++ ConsoleApp1/Niz.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Niz
+    {
+        public static int GetNumberFromConsole()
+        {
+            int result;
+            string entry = Console.ReadLine();
+            while (!int.TryParse(entry, out result))
+            {
+                Console.Write("Please type a numeric value: ");
+                entry = Console.ReadLine();
+            };
+            return result;
+        }
+
+        public static int[] CreateArray()
+        {
+            Console.WriteLine("Please specify the array length:");
+            int length = GetNumberFromConsole();
+            int[] array = new int[length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Element {i + 1}: ");
+                array[i] = GetNumberFromConsole();
+            }
+            return array;
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            Console.WriteLine("Printing all elements of the array:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Element {i + 1}: {array[i],5}");
+            }
+        }
+
+        public static float CalculateMeanDivisible(int[] array)
+        {
+            int sum = 0;
+            int numberOfDivisibleElements = 0;
+            foreach (int num in array)
+            {
+                if (num%5 == 0)
+                {
+                    numberOfDivisibleElements++;
+                    sum = sum + num;
+                }
+            }
+            float result = (float)sum / (float)numberOfDivisibleElements;
+            return result;
+        }
+
+        public static int[] SortArrayAscending(int[] array)
+        {
+            int temp;
+            var len = array.Length;
+            for (var i = 1; i < len; i++)
+            {
+                for (var j = 0; j < len - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+
+        public static int FindSmallestElement(int[] array)
+        {
+            int min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+
+        public static int[] CombineArrays(int[] array1, int[] array2)
+        {
+            int totalLength = array1.Length + array2.Length;
+            int[] resultArray = new int[totalLength];
+            for (int i = 0; i < array1.Length; i++)
+            {
+                resultArray[i] = array1[i];
+            }
+            for (int i = 0; i < array2.Length; i++)
+            {
+                resultArray[array1.Length+i] = array2[i];
+            }
+            return resultArray;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating of an array
+            //int[] array1 = { 1, 55, 99, 15, 10, -30, 87 };
+            int[] array0 = Niz.CreateArray();
+            // task 1:
+            // Write a Function  for calculating the mean value of those elements of an array of integers that are divisible by 5.
+            Console.WriteLine("\nTask 1\n");
+            Console.WriteLine("Mean value of elements divisible by 5 = {0:0.##}", Niz.CalculateMeanDivisible(array0));
+
+            // task 2:
+            // Write a method  for finding the smallest and largest values of an array of integers.
+            Console.WriteLine("\nTask 2\n");
+            Console.WriteLine("The smallest element of the array: {0}", Niz.FindSmallestElement(array0));
+
+            // task 3
+            // Write a method for sorting an array in ascending order.
+            Console.WriteLine("\nTask 3\n");
+            int[] sortedArray = Niz.SortArrayAscending(array0);
+            Console.WriteLine("Sorted array:");
+            Niz.PrintArray(sortedArray);
+
+            // task 4:
+            // Write a method to combine two arrays into a third. All three arrays must be in ascending order
+            Console.WriteLine("\nTask 4\n");
+            Console.WriteLine("Create the first array - ");
+            int[] array1 = Niz.CreateArray();
+            Console.WriteLine("Create the second array - ");
+            int[] array2 = Niz.CreateArray();
+
+            Console.WriteLine("\nSorting arrays:");
+            array1 = Niz.SortArrayAscending(array1);
+            array2 = Niz.SortArrayAscending(array2);
+            Console.WriteLine("Array1 after sorting:");
+            Niz.PrintArray(array1);
+            Console.WriteLine("Array2 after sorting:");
+            Niz.PrintArray(array2);
+
+            Console.WriteLine("\nCombining arrays:");
+            int[] array3 = Niz.CombineArrays(array1, array2);
+            Niz.PrintArray(array3);
+
+            Console.WriteLine("\nCombinedArray after sorting:");
+            array3 = Niz.SortArrayAscending(array3);
+            Niz.PrintArray(array3);
+
+            // task 4:
+            // Switch-case
+            Console.WriteLine("\nTask 5\n");
+            var defaultColor = Console.BackgroundColor;
+            string answer;
+            do
+            {
+                Console.WriteLine("Choose a color for background or exit:");
+                Console.WriteLine("\tBlack");
+                Console.WriteLine("\tRed");
+                Console.WriteLine("\tGreen");
+                Console.WriteLine("\tBlue");
+                Console.WriteLine("\tDark");
+                Console.WriteLine("\tExit");
+                answer = Console.ReadLine().ToLower();
+                switch (answer)
+                {
+                    case "black":
+                    case "dark":
+                        {
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.WriteLine("Check");
+                            break;
+                        }
+                    case "red":
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine("Check");
+                            break;
+                        }
+                    case "green":
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGreen;
+                            Console.WriteLine("Check");
+                            break;
+                        }
+                    case "blue":
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkBlue;
+                            Console.WriteLine("Check");
+                            break;
+                        }
+                    case "exit":
+                        {
+                            Console.BackgroundColor = defaultColor;
+                            Console.WriteLine("Exiting...");
+                            break;
+                        }
+                    default:
+                        {
+                            Console.BackgroundColor = defaultColor;
+                            Console.WriteLine("Sorry, not implemented =(");
+                            break;
+                        }
+                }    
+            }
+            while (answer != "exit");
+
+            Console.ReadLine();
+        }
+    }
+}
